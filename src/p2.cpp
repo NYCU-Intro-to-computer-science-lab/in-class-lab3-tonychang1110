9d617eca5d3f49c59ccc0d97da8db91c20f5e55e
tonychang1110 <tonychang1110.mg14@nycu.edu.tw>
2025-11-25 13:53:05 +0800

    Add GCD function to calculate greatest common divisor

--- src/p2.cpp
+++ src/p2.cpp
@@ -1,10 +1,14 @@
-// 題目 2：完美鋪磚計畫 (The Perfect Tiling Project) - 20分
-
 #include <iostream>
 using namespace std;
 
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
 int main() {
-    
-    
+    int num1, num2;
+    cin >> num1 >> num2;
+    cout << gcd(num1, num2) << endl;
     return 0;
 }
