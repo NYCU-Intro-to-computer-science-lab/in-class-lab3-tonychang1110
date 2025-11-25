e588d9d63f123faf30b459fa28270441fde7f3d3
tonychang1110 <tonychang1110.mg14@nycu.edu.tw>
2025-11-25 13:50:44 +0800

    Update p1.cpp

--- src/p1.cpp
+++ src/p1.cpp
@@ -1,10 +1,28 @@
-// 題目 1：高塔建築師 (The Tower Architect) - 20分
-
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main() {
-    
-    
+long long factorial(int n)
+{
+    if(n <= 1)
+        return 1;
+    return n * factorial(n - 1);
+}
+
+long long sum(int n)
+{
+    if(n <= 0)
+        return 0;
+    return n + sum(n - 1);
+}
+
+int main(){
+    int number;
+    cin >> number;
+
+    long long summation = sum(number);
+
+    cout << factorial(number) <<endl;
+    cout << sum(number) << endl;
+
     return 0;
 }
